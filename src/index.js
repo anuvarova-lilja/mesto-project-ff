0b4745d7cca3250e3c437c06225a2ffd491074f4
Lilja Anuvarova <lila2m@yandex.ru>
2024-03-10 13:00:10 +0500

    1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-// @todo: Темплейт карточки
-// @todo: DOM узлы
-// @todo: Функция создания карточки
-// @todo: Функция удаления карточки
-// @todo: Вывести карточки на страницу
-
+import './index.css';
+import {initialCards} from '../scripts/cards.js';
 
 const cardTemplate = document.querySelector('#card-template').content;
 
---
+++ src/scripts/cards.js
@@ -0,0 +1,49 @@
+
+//  const initialCards = [
+//     {
+//       name: "Архыз",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+//     },
+//     {
+//       name: "Челябинская область",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+//     },
+//     {
+//       name: "Иваново",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+//     },
+//     {
+//       name: "Камчатка",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+//     },
+//     {
+//       name: "Холмогорский район",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+//     },
+//     {
+//       name: "Байкал",
+//       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+//     }
+// ];
+
+// export initialCards;
+
+// теперь картинки можно импортировать,
+// вебпак добавит в переменные правильные пути
+const arkhyzImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url);
+const chelyabinskImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', import.meta.url);
+const ivanovoImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', import.meta.url);
+const kamchatkaImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', import.meta.url);
+const kholmogorskyImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', import.meta.url);
+const baikalImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', import.meta.url);
+
+
+export const initialCards = [
+  // меняем исходные пути на переменные
+  { name: 'Архыз', link: arkhyzImage },
+  { name: 'Челябинская область', link: chelyabinskImage },
+  { name: 'Иваново', link: ivanovoImage },
+  { name: 'Камчатка', link: kamchatkaImage },
+  { name: 'Холмогорский район', link: kholmogorskyImage },
+  { name: 'Байкал', link: baikalImage },
+];
