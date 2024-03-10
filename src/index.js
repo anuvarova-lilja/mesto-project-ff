812a2e9824bc0f8fa83036eac9123bbb1f672e02
Lilja Anuvarova <lila2m@yandex.ru>
2024-03-10 22:37:04 +0500

    2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
-import './index.css';
-import {initialCards} from '../scripts/cards.js';
+import "./index.css";
+import { initialCards } from "./scripts/cards.js";
 
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardContainer = document.querySelector('.places__list');
+const cardContainer = document.querySelector(".places__list");
 
-function createCard (link, name, altText, {deleteCard}) {
-    const cardElem = cardTemplate.querySelector('.places__item').cloneNode(true);
-    
-    const cardImage = cardElem.querySelector(".card__image");
-    cardImage.src = link;
-    cardElem.querySelector('.card__title').textContent = name;
-    cardImage.alt = altText;
+function createCard(link, name, altText, { deleteCard }) {
+  const cardElem = cardTemplate.querySelector(".places__item").cloneNode(true);
 
-    const cardDelBtn = cardElem.querySelector('.card__delete-button');
-    cardDelBtn.addEventListener('click', deleteCard);
+  const cardImage = cardElem.querySelector(".card__image");
+  cardImage.src = link;
+  cardElem.querySelector(".card__title").textContent = name;
+  cardImage.alt = altText;
 
-    return cardElem;
+  const cardDelBtn = cardElem.querySelector(".card__delete-button");
+  cardDelBtn.addEventListener("click", deleteCard);
+
+  return cardElem;
 }
 
-function addCard (item, {deleteCard}) {
-    const cardElem = createCard(item.link, item.name, item.altText, {deleteCard});
-    cardContainer.append(cardElem);
-};
-
+function addCard(item, { deleteCard }) {
+  const cardElem = createCard(item.link, item.name, item.altText, {
+    deleteCard,
+  });
+  cardContainer.append(cardElem);
+}
 
 function deleteCard(evt) {
-    const delBtn = evt.target.closest('.places__item');
-    delBtn.remove();
+  const delBtn = evt.target.closest(".places__item");
+  delBtn.remove();
 }
 
-initialCards.forEach(function(item) {
-    addCard(item, {deleteCard});
+initialCards.forEach(function (item) {
+  addCard(item, { deleteCard });
 });
+
+const editButton = document.querySelector('.profile__edit-button');
+const editWindow = document.querySelector('.popup_type_edit');
+
+editButton.addEventListener('click', openPopup);
+editWindow.addEventListener('click', closePopup);
+
+function openPopup() {
+    editWindow.classList.add('popup_is-opened');
+};
+
+function closePopup() {
+    // const target = evt.target;
+    // if (target === editWindow) {
+        // 
+    editWindow.classList.add('popup__close');
+};
